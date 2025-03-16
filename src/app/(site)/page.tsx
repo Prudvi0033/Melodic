d6b29c542b5192964d24615cb091b42e06c2faf0
Prudvi <prudvirajvarma2004@gmail.com>
2025-03-16 22:28:54 +0530

    Added Lib

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -12,10 +12,18 @@ const page = () => {
         <div className='mb-2'>
           <h1 className='text-white text-3xl font-semibold'>Welcome back!</h1>
           <div className='grid grid-cols-1 sm:grid-cols-1 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4'>
-            <ListIem/>
+            <ListIem image='/liked.png' name='Liked Songs' href='liked' />
           </div>
         </div>
       </Header>
+      <div className='mt-2 mb-7 px-6'>
+        <div className='flex justify-items-center'>
+          <h1 className='text-white font-medium text-2xl'>Newest Songs</h1>
+        </div>
+        <div>
+          List of Songs!
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/components/ListIem.tsx
+++ src/app/components/ListIem.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { Play } from 'lucide-react'
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
@@ -15,9 +18,13 @@ const ListIem = ({image, name, href} : ListIemProps) => {
         router.push(href)
     }
   return (
-    <button className='relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4'>
-        <div>
-            
+    <button onClick={handleClick} className='relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4'>
+        <div className='relative min-h-[64px] min-w-[64px]'>
+            <Image className='object-cover' fill src={image} alt='Liked' />
+        </div>
+        <p className='text-lg font-medium truncate py-5'>{name}</p>
+        <div className=' absolute transition opacity-0 flex justify-items-center rounded-full bg-blue-500 p-3 drop-shadow-md right-5 group-hover:opacity-110 hover:scale-110'>
+            <Play className='text-white'/>
         </div>
     </button>
   )
